49b4325740818c033c78635ec67ac3e40db5119d
clarteweb35 <clarte.web35@gmail.com>
2026-07-19 00:18:18 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://clarteweb35.github.io',
+  base: '/autour-de-rennes'
+});
