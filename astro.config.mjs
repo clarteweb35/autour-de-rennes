198019f6e20fb8b4917290b84634fa66618d0658
clarteweb35 <clarte.web35@gmail.com>
2026-07-21 00:07:10 +0200

    Ajout sitemap + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://clarteweb35.github.io',
-  base: '/autour-de-rennes'
+  base: '/autour-de-rennes',
+  integrations: [sitemap()]
 });
